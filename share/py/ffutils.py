84ba2444c60e0c9a7131f401eba209892524209e
Darren Embry <dse@webonastick.com>
2024-10-08 21:53:31 -0400

    standard utils

---
+++ share/py/ffutils.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+stderr_fd = None
+
+def import_fontforge():
+    global stderr_fd, fontforge
+    if not os.environ.get("FFUTILS_DEBUG"):
+        stderr_fd = os.dup(2)
+        os.close(2)
+    import fontforge
+    if not os.environ.get("FFUTILS_DEBUG"):
+        os.dup2(stderr_fd, 2)
+
+def load_font(filename):
+    global stderr_fd, fontforge
+    try:
+        if not os.environ.get("FFUTILS_DEBUG"):
+            os.close(2)
+        font = fontforge.open(filename, ('fstypepermitted',))
+        if not os.environ.get("FFUTILS_DEBUG"):
+            os.dup2(stderr_fd, 2)
+        return font
+    except OSError as e:
+        if not os.environ.get("FFUTILS_DEBUG"):
+            os.dup2(stderr_fd, 2)
+        sys.stderr.write("%s: %s\n" % (filename, e))
+        return None
