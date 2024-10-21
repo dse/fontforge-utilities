4b78cc2a503be77cb2e45eb86e44c1b52f1dc617
Darren Embry <dse@webonastick.com>
2024-10-21 19:26:29 -0400

    add methods

--- share/py/ffutils.py
+++ share/py/ffutils.py
@@ -28,6 +28,27 @@ def load_font(filename):
         sys.stderr.write("%s: %s\n" % (filename, e))
         return None
 
+def font_is_mono(font, mostly=False, percentile=95):
+    (percentage, mono_count, glyph_count, top_width) = top_glyph_width_percentage(font)
+    return (percentage == 100) or (mostly and percentage >= percentile)
+
+def top_glyph_width_percentage(font):
+    width_counts = {}
+    for glyph in font.glyphs():
+        width = glyph.width
+        if width in width_counts:
+            width_counts[width] += 1
+        else:
+            width_counts[width] = 1
+    if len(width_counts) < 1:
+        return 100
+    widths = list(width_counts.keys())
+    widths.sort(reverse=True, key=lambda width: width_counts[width])
+    return (width_counts[widths[0]] / len(list(font.glyphs())) * 100,
+            width_counts[widths[0]],
+            len(list(font.glyphs())),
+            widths[0])
+
 def u(codepoint):
     if codepoint < 0:
         return "%-8d" % codepoint
